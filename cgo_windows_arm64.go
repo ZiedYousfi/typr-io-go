c1f4eaff7b24c99cf1ac3ba93b19e7727966873c
ZiedYousfi <yousfi_zied@yahoo.com>
2025-12-29 13:34:26 +0100

    fix: add missing LDFLAGS for stdc++ and lm in Windows cgo files

--- cgo_windows_arm64.go
+++ cgo_windows_arm64.go
@@ -3,6 +3,6 @@
 package typrio
 
 /*
-#cgo windows,arm64 LDFLAGS: -L${SRCDIR}/lib/windows-arm64 -ltypr_io -luser32
+#cgo windows,arm64 LDFLAGS: -L${SRCDIR}/lib/windows-arm64 -ltypr_io -luser32 -lstdc++ -lm
 */
 import "C"
