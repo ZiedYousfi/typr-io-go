44ed07291a6f9ba14eeb45bddb7de6feb5e1cc11
ZiedYousfi <yousfi_zied@yahoo.com>
2025-12-30 13:03:50 +0100

    fix: update library references from typr_io to axidev_io in cgo files for Windows builds

--- cgo_windows_amd64.go
+++ cgo_windows_amd64.go
@@ -3,6 +3,6 @@
 package axidevio
 
 /*
-#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/lib/windows-x64 -ltypr_io -luser32 -lstdc++ -lm
+#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/lib/windows-x64 -laxidev_io -luser32 -lstdc++ -lm
 */
 import "C"
